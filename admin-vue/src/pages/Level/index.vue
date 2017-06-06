484b01fc9fa43fca62ee24d06598abfc26703d72
Martin <cauu@163.com>
2017-06-07 00:04:52 +0800

    add folder

---
+++ admin-vue/src/pages/Level/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Table
+      :columns="columns"
+      :data="levels"
+      >
+    </Table>
+  </div>
+</template>
+
+<script>
+  import { mapState, mapMutations } from 'vuex'
+
+  export default {
+    data() {
+    },
+    computed: mapState({
+    }),
+    methods: {
+    }
+  }
+</script>
+
+<style>
+</style>
